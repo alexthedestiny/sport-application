c9bd86c7e44e03d96d3189b97571311a4b87ec47
Oleksandr <oleksandrpavlenko@yahoo.com>
2017-03-12 22:47:37 +0200

    made a log of changes with gMap

--- app/components/index-page/index-page.component.js
+++ app/components/index-page/index-page.component.js
@@ -5,18 +5,40 @@ angular.
   module('indexPage').
   component('indexPage', {
     templateUrl: 'components/index-page/index-page.template.html',
-    controller: ['$scope', 'Movie',
-      function MovieListController($scope, Movie) {
+    controller: ['$scope', 'Movie', '$filter',
+      function MovieListController($scope, Movie, $filter) {
+
             google.maps.visualRefresh = true;
-            $scope.markers = [{
+            $scope.sub_cat = [
+                {title: "Футбольные поля", id_cat: 1},
+                {title: "Баскетбольные поля", id_cat: 1},
+                {title: "Волейбольные поля", id_cat: 2},
+                {title: "Теннисные корты", id_cat: 2}
+            ];
+            // $scope.selectedItem = $scope.sub_cat[0];
+            $scope.selectedItem = null;
+            $scope.searchField = function(item) {
+                console.log($filter);
+                return true;
+            };
+            $scope.id_active_cat = 1;
+            $scope.changeIdCat = function (id_cat) {
+                $scope.id_cat = id_cat;
+                return $scope.id_cat;
+            };
+            $scope.markers = [
+            {
                 id:0,
                 latitude: 50.450645,
                 longitude: 30.523180,
                 showWindow: false,
                 draggable: true,
-                title: 'asd',
+                title: 'Супер клуб',
+                description: 'Супер-пупер офигенный клуб',
+                id_cat: 1,
                 opts: {
-                    draggable: true
+                    draggable: true,
+                    icon:'../img/Stadium-48.png'
                 },
                 events: {
                     dragend: function (marker) {
@@ -31,11 +53,40 @@ angular.
                         });
                     }
                 }
-            }];
+            },
+            {
+                id:1,
+                latitude: 50.650645,
+                longitude: 30.523180,
+                showWindow: false,
+                draggable: true,
+                title: 'Супер помещение',
+                description: 'Супер-пупер офигенный клубешник',
+                id_cat: 2,
+                opts: {
+                    draggable: true,
+                    icon:'../img/Basketball-Field-48.png'
+                },
+                events: {
+                    dragend: function (marker) {
+                        $scope.$apply(function () {
+                            $scope.markers.latitude = marker.getPosition().lat().toFixed(5);
+                            $scope.markers.longitude = marker.getPosition().lng().toFixed(5);
+                        });
+                    },
+                    click: function (marker) {
+                        $scope.$apply(function () {
+                            $scope.markers.splice(marker.key, 1);
+                        });
+                    }
+                }
+            }
+
+            ];
             $scope.map = {
               control: {},
               center: { latitude: 50.450645, longitude: 30.523180 },
-              zoom: 14,
+              zoom: 9,
               showWindow: false,
               addSFewMarkers: true,
               onClick: function (mapObj, event, eventArguments) {
@@ -74,7 +125,7 @@ angular.
                           }
                       }
                   });
-                  $scope.$apply();
+                  // $scope.$apply();
               }
           };
       }
--- app/components/index-page/index-page.module.js
+++ app/components/index-page/index-page.module.js
@@ -6,4 +6,37 @@ angular.module('indexPage', [
     'header',
     'uiGmapgoogle-maps',
     'footer'
-]);
+])
+    .filter('customSearchPlaces',[function(){
+        return function(data,f){
+            var output = [];
+            if(!!f){
+                for(var i = 0;i<data.length; i++){
+                    if(data[i].title.toLowerCase().indexOf(f.toLowerCase()) !== -1 || data[i].description.toLowerCase().indexOf(f.toLowerCase()) !== -1){
+                        output.push(data[i]);
+                    }
+                }
+            }
+            else {
+                output = data;
+            }
+            return output;
+        }
+    }])
+    .filter('sortByIdCat',[function(){
+        return function(data,id_cat){
+            var output = [];
+            if(id_cat != null){
+                for(var i = 0;i<data.length; i++){
+                    if(data[i].id_cat == id_cat){
+                        output.push(data[i]);
+                    }
+                }
+            }
+
+            else {
+                output = data;
+            }
+            return output;
+        }
+    }]);
